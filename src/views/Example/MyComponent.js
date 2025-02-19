2b67c2c919aeafba5f5f3154692fef76beb33996
Nguyen Truong Nhat Anh <nhatanh2629@gmail.com>
2025-02-19 20:47:48 +0700

    test buoi 4

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MyComponent extends React.Component{
+    render(){
+        return(
+            <div>hello my Component Nguyen Truong Nhat Anh</div>
+        )
+    }    
+}
+
+export default MyComponent;
